a414902270fec441aa57dade6170bb3a2546312d
wagner <wggauer@gmail.com>
2023-05-27 12:35:31 -0300

    add ingress and skaffold yaml files

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,19 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there!");
+});
+
+app.use("/api/users", (req, res) => {
+  res.send("In the / route");
+});
+
+app.use("/", (req, res) => {
+  res.send("In the / route");
+});
+
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log("Dm          e?");
+  console.log("List ening on port 3000");
 });
