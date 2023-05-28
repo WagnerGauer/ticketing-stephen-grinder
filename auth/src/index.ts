99561f4f0be93ba9ffd817273900b8a11c53834d
wagner <wggauer@gmail.com>
2023-05-28 07:24:36 -0300

    create routes folder and files, link each router to index.ts, add express-validatorlibrary

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,21 +1,29 @@
 import express from "express";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
 const app = express();
 app.use(express.json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Bye dog there!");
-});
-
-app.use("/api/users", (req, res) => {
-  res.send("In the / route");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.use("/", (req, res) => {
-  res.send("In the / route");
+  res.send("No route hit");
+});
+app.get("/", (req, res) => {
+  res.send("No route hit");
+});
+app.post("/", (req, res) => {
+  res.send("No route hit");
 });
 
 app.listen(3000, () => {
-  console.log("Dme?");
+  console.log("Updated again");
   console.log("List ening on port 3000");
 });
