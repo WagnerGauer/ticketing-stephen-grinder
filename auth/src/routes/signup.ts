d8c0891f2dfedd872041c38091480a6c1051b039
wagner <wggauer@gmail.com>
2023-05-29 03:41:49 -0300

    add new generic error class to handle bad request errors, use class in signup.ts

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+
+import { User } from "../models/user";
+
 import { RequestValidatonError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -14,7 +17,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -23,10 +26,16 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log("Creating a user");
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      throw new BadRequestError("Email is in use");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
