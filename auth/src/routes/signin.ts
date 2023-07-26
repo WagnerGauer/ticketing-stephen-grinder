1091cc4920b46659bf81ad8e0df56c6463e1009f
Wagner <wggauer@gmail.com>
2023-07-25 22:20:17 -0300

    check user credentials for the login route and send an error if the credentials don't match or send a cookie with a jwt token and the user in the as a response if credentials match

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
 
+import { Password } from "../services/password";
+import { User } from "../models/user";
 import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -15,7 +19,42 @@ router.post(
       .withMessage("You must supply a password"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+      // A check to see if this evironment variable defined was already put in place in the start
+      // function in the index.ts file, by putting ! at the end there I can make typescript stop complaining
+    );
+
+    // Store it on a session object
+    req.session = {
+      jwt: userJwt,
+    };
+    console.log(req.session);
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
