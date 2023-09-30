e4ea4c9c26369ed6b8708c966c8cf41f478b26c9
Wagner <wggauer@gmail.com>
2023-09-30 20:44:47 -0300

    add axios to the project, add signup.js file and create code to send a request to signup to the api

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import axios from "axios";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await axios.post("/api/users/signup", {
+      email,
+      password,
+    });
+
+    console.log(response.data);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        ></input>
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
+
+export default Signup;
