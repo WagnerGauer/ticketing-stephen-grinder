7871453e2baafceb586f5f3a32e596ee8c095196
Wagner <wggauer@gmail.com>
2023-09-30 22:57:09 -0300

    use the getInitialProps method which runs in ther server to fetch properties to be passes down to the component, understand how to send a request from one container to other in ther server

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,31 @@
-const Landing = () => {
-  console.log("this is a test");
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing pagsfe</h1>;
 };
 
-export default Landing;
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    // we are on the server!
+
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+
+    console.log(data);
+    return data;
+  } else {
+    // we are in the browser!
+    const { data } = await axios
+      .get("/api/users/currentuser")
+      .catch((err) => console.log(err));
+    return data;
+  }
+};
+
+export default LandingPage;
