bc594bdba53aa619c5b0b25189e851d01a0549f5
Wagner <wggauer@gmail.com>
2023-10-09 22:40:10 -0300

    create Uniform Resource Identifier variables for the pods runnig the database for the auth and tickets service, use these variables in each service's respecitive index.js files

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,8 +6,11 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
+  if (!process.env.AUTH_MONGO_URI) {
+    throw new Error("JWT_KEY must be defined");
+  }
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.AUTH_MONGO_URI);
     console.log("Connected to mongodb");
   } catch (err) {
     console.error(err);
