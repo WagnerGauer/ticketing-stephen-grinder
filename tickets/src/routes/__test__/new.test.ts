b060d0b19f21b694e05613a62c3435aaae4432f0
Wagner <wggauer@gmail.com>
2023-10-11 16:51:34 -0300

    add tests to make sure that a status code of 400 is sent when an invalid title or price is provided

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -18,10 +18,31 @@ it("returns a status other than 401 if the user is signed in", async () => {
     .set("Cookie", global.signin())
     .send({});
 
-  // console.log(response.status);
   expect(response.status).not.toEqual(401);
 });
 
-// it("returns an error if an invalid title is provided", async () => {});
-// it("returns an error if an ivalid price is provided", async () => {});
-// it("creates a ticket with valid inputs", async () => {});
+it("returns an error if an invalid title is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({ title: "", price: 10 })
+    .expect(400);
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({ price: 10 })
+    .expect(400);
+});
+it("returns an error if an ivalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({ title: "sfasdfadfsa", price: -10 })
+    .expect(400);
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({ title: "sfasdfadfsa" })
+    .expect(400);
+});
+it("creates a ticket with valid inputs", async () => {});
