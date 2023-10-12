3f452e6cb0f1dbabdef9c4c02b657e096cbe2c2f
Wagner <wggauer@gmail.com>
2023-10-12 18:52:29 -0300

    create test to test if all tickets are being retrieved and if a 200 status code is being sent, create route to get all tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@wagtickets/common";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
 
 const app = express();
 app.set("trust proxy", true);
@@ -19,6 +20,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all("*", (req, res) => {
   console.log("this route does not exist in the server");
